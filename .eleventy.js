627adcda2b736e78bd779dd8faa8fde2a95d9dba
Prabhakar Kafle <pkafleprabhakar@gmail.com>
2022-07-09 15:04:15 -0700

    add bootstrap, nav plugin, and configs

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
 		files: './_site/css/**/*.css'
 	});
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
     return {
         dir: {
             input: "src"
-        }
+        },
+        "dataTemplateEngine": "njk",
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        templateFormats: ["html", "md", "njk", "11ty.js"]
     }
 };
