95843f8c1e9265f97b1d10cc7ef1a0c7e02027e0
Prabhakar Kafle <pkafleprabhakar@gmail.com>
2022-07-24 16:40:01 -0700

    add rebuilding blog and layout

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,14 @@ module.exports = function(eleventyConfig) {
 	});
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    eleventyConfig.addNunjucksFilter("getDateString", function(date_obj) {
+        console.log({date_obj});
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        const date = date_obj.getUTCDate();
+        const month = date_obj.getUTCMonth();
+        const year = date_obj.getUTCFullYear();
+        return `${months[month]} ${date}, ${year}`;
+    });
     eleventyConfig.addPassthroughCopy("src/images");
     return {
         dir: {
